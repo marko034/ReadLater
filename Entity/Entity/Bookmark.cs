02f91d82e9bac163f21497df87611573a34f29b4
marko034 <77991794+marko034@users.noreply.github.com>
2022-09-11 11:10:58 +0200

    Add files via upload

---
+++ Entity/Entity/Bookmark.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Bookmark
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(maximumLength: 500)]
+        public string URL { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        [ForeignKey("CreatedByUser")]
+        public string CreatedByUserId { get; set; }
+        public IdentityUser CreatedByUser { get; set; }
+    }
+}
